479004de3ce5973abfbe6b997398a257a6fc475e
Marlon Fernandez <marlonfsolis@outlook.com>
2020-08-31 20:48:17 -0400

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header></Header>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item"
+              v-for="index in 5" :key="index">
+            <ServerItem :serverId="index"></ServerItem>
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <ServerDetails></ServerDetails>
+      </div>
+    </div>
+    <hr>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import ServerItem from "@/components/ServerItem.vue";
+
 
 export default {
   name: 'App',
+
+  // registering components local. Only for this component.
+  // Will need to register in other components individually if needed.
   components: {
-    HelloWorld
+    Header,
+    Footer,
+    ServerItem
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ServerDetails from "@/components/ServerDetails";
 
 Vue.config.productionTip = false
+
+// registering the component global. Will be available for entire application.
+Vue.component('ServerDetails', ServerDetails);
 
 new Vue({
   render: h => h(App),
